bccce2e551ada671acafe4cbeee5e6dbd8d45c09
Kamonnun Silarat <102642408+Trailbblazer@users.noreply.github.com>
2023-08-29 13:43:40 +0300

    Add files via upload

---
+++ Kamonnun_mathinput.py
@@ -0,0 +1,4 @@
+g = float(input("enter the value of acceleration due to gravity (in m/s^2): "))
+m = float(input("enter the mass of an object (in kg): "))
+w = m * g
+print(f"Weight: {float(w):.1f} kg")
